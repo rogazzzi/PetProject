90e4e38ee78497e22831f4c014fbc23d57694833
dna <dsantamanta@gmail.com>
2024-12-25 22:10:28 +0700

    add auto start server

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,14 @@ public class Main {
     public static void main(String[] args) {
         // get started server api on new thread
         Thread server_thread = new Thread(new StartServer());
-        server_thread.start();
         // get started server api on new thread
-        SomeInterface();
-        if (server_thread.isAlive()) {
+        while (true) {
+            SomeInterface(server_thread);
+        }
+        /*if (server_thread.isAlive()) {
             server_thread.interrupt();
             System.out.println("server shutdown");
-        }
+        }*/
         //unused
 //        ClientMain ac = new ClientMain();
 //        StringBuilder content = ac.SendRequest("Test");
@@ -22,8 +23,18 @@ public class Main {
         //unused
     }
 
-    public static void SomeInterface(){
+    public static void SomeInterface(Thread server_thread){
 
+        if (server_thread.isAlive()) {
+            server_thread.interrupt();
+            System.out.println("server shutdown");
+        }
+        else{
+            server_thread.start();
+            if (server_thread.isAlive()) {
+                System.out.println("server start");
+            }
+        }
         while (true){
             System.out.println("1 - Test; 2- TestDB");
             try {
@@ -45,6 +56,4 @@ public class Main {
             }
         }
     }
-
-
 }
